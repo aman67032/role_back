f509934f47d297978eb867f06384a6b7c3116314
Aman Pratap Singh <amanpratapsingh@jklu.edu.in>
2026-03-20 18:03:12 +0530

    rest

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Booking = require('./models/Booking');
+const Booking = require('./models/Booking.js');
 
 dotenv.config();
 
